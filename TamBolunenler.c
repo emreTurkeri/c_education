49c8f5b90461d0b1dfcb3fb0602af1bfdd31bd8b
emreTurkeri <112562188+emreTurkeri@users.noreply.github.com>
2022-12-12 09:35:28 +0300

    Create TamBolunenler.c

---
+++ TamBolunenler.c
@@ -0,0 +1,13 @@
+int main(){
+  //Bu fonksiyon 2 ve 3'e tam bölünen sayılar vardır.
+  //10 ile 100 arasındakileri verir.
+  tamBolunenler();
+}
+
+void tamBolunenler(int ) {
+    for (int i = 10; i <=1nl00 ; ++i) {
+        if(i%2 == 0 && i%3 == 0 )
+            printf("%d ",i);
+    }
+}
+
